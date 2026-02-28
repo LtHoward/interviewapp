414184db5f20a1c328e11ea5befd7d9aba49e90b
GitGud9275 <tjhamilton738@gmail.com>
2026-02-28 14:24:37 -0500

    dataConstants class added

---
+++ src/main/java/com/model/dataConstants.java
@@ -0,0 +1,62 @@
+package com.model;
+
+public final class dataConstants {
+
+    private dataConstants() { }
+
+    public static final String USERS_FILE = "json/users.json";
+    public static final String POSTS_FILE = "json/posts.json";
+
+    public static final String USER_ID = "id";
+    public static final String USERNAME = "username";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String ROLE = "role";
+
+    public static final String STUDENT_DATA = "studentData";
+    public static final String CURRENT_CLASSES = "currentClasses";
+    public static final String CLASSES_TAKEN = "classesTaken";
+    public static final String MAJOR = "major";
+    public static final String YEAR = "year";
+    public static final String EXPERIENCE = "experience";
+    public static final String SOLVED_QUESTIONS = "solvedQuestions";
+    public static final String POSTED_SOLUTIONS = "postedSolutions";
+    public static final String LAST_ACTIVITY_DATE = "lastActivityDate";
+
+    public static final String CONTRIBUTOR_DATA = "contributorData";
+    public static final String EXPERIENCE_DESCRIPTION = "experienceDescription";
+
+    public static final String PROGRESSION = "progression";
+    public static final String POINTS = "points";
+    public static final String LEVEL = "level";
+    public static final String CURRENT_STREAK = "currentStreak";
+    public static final String LONGEST_STREAK = "longestStreak";
+    public static final String EQUIPPED_TITLE = "equippedTitle";
+    public static final String UNLOCKED_TITLES = "unlockedTitles";
+
+    public static final String REWARDS = "rewards";
+    public static final String TYPE = "type";
+    public static final String AMOUNT = "amount";
+    public static final String REDEEMED = "redeemed";
+
+    public static final String POST_TYPE = "type";
+    public static final String TITLE = "title";
+    public static final String AUTHOR_ID = "authorId";
+    public static final String CREATED_AT = "createdAt";
+    public static final String DIFFICULTY = "difficulty";
+    public static final String HINT = "hint";
+    public static final String TAGS = "tags";
+    public static final String SCORE = "score";
+    public static final String CONTENT_SECTIONS = "contentSections";
+    public static final String COMMENTS = "comments";
+
+    public static final String QUESTION_ID = "questionId";
+    public static final String SOLUTION_NUMBER = "solutionNumber";
+
+    public static final String CONTENT = "content";
+
+    public static final String QUESTION = "QUESTION";
+    public static final String SOLUTION = "SOLUTION";
+}
