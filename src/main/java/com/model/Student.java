9a2a638866f794fc504f639c06036d6ceae5e11e
MyilaHoward <myila@email.sc.edu>
2026-02-28 16:36:01 -0500

    adding #11

---
+++ src/main/java/com/model/Student.java
@@ -0,0 +1,44 @@
+package com.model;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.Date;
+
+public class Student extends User{
+    private String currentClasses;
+    private String classesTaken;
+    private Major major;
+    private Year year;
+    private Experience experience;
+    private int solvedQuestions;
+    private ArrayList<SolutionPost> postedSolutions;
+    private Progression progression;
+    private ArrayList<Reward> rewards;
+    private Date lastActivityDate;
+
+    public Student (UUID id, String username, String email, String password, String firstName, String lastName, Major major, Year year) {
+        super(id, username, email, password, firstName, lastName);
+        
+        
+    }
+
+    public void recordSolvedQuestions(){
+
+    }
+
+    public int addPoints() {
+        return 0;
+    }
+
+    public ArrayList<SolutionPost> getPostedSolution() {
+        return null;
+    }
+
+    public void updateLevel() {
+
+    }
+
+    public Date getCurrentStreak() {
+        return null;
+    }
+    
+}
