2938c9d8b4b4045e60146492d1997a10f89b32a7
GitGud9275 <tjhamilton738@gmail.com>
2026-02-28 16:52:10 -0500

    #6 fixed QuestionPost class stub

--- src/main/java/com/model/QuestionPost.java
+++ src/main/java/com/model/QuestionPost.java
@@ -1,29 +1,35 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 public class QuestionPost extends Post {
-    
-    public class QuestionPost extends Post {
 
     private String title;
     private Difficulty difficulty;
     private String hint;
 
-    public QuestionPost(UUID id,
-                        User author,
-                        Date createdAt,
-                        ArrayList<Comment> comments,
-                        ArrayList<String> tags,
-                        ArrayList<PostContent> contentSections,
-                        int score,
+    public QuestionPost(User author,
                         String title,
                         Difficulty difficulty,
                         String hint) {
 
-        super(id, author, createdAt, comments, tags, contentSections, score);
+        super(author);
+        this.title = title;
+        this.difficulty = difficulty;
+        this.hint = hint;
     }
-}
 
+    public String getTitle() {
+        return title;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public String getHint() {
+        return hint;
+    }
 }
