2361a62d2d4d1ee10a71a88c774e8e7cf972df8f
MyilaHoward <myila@email.sc.edu>
2026-03-22 17:12:39 -0400

    documentation for #75

--- src/main/java/com/model/InterviewApp.java
+++ src/main/java/com/model/InterviewApp.java
@@ -2,12 +2,19 @@ package com.model;
 
 import java.util.ArrayList;
 
+/**
+ * Class to represent the interview application.
+ * This class manages the users, posts, questions, solutions, and comments.
+ */
 public class InterviewApp {
     private PostManager postManager;
     private ArrayList<Post> savedPost;
     private UserManager userManager;
     private User currentUser;
 
+    /**
+     * Construtor that initializes the managers and saved posts 
+     */
     public InterviewApp() 
     {        
         postManager = PostManager.getInstance();
@@ -17,22 +24,41 @@ public class InterviewApp {
 
     }
 
+    /**
+     * Method to remove a user
+     * @param user the user to be removed 
+     * @return true if the user was successfuly removed, false otherwise
+     */
     public boolean removeUser(User user)
     {
       if (user == null) return false;
       return userManager.removeAccount(user);
     }
 
+    /**
+     * Method to get the user in the userlist
+     * @return the users in the userlist
+     */
     public ArrayList<User> getAllUsers()
     {
         return userManager.getAllUsers();
         
     }
 
+    /**
+     * Method to get all the post in the post list
+     * @return all the post in the post list
+     */
     public ArrayList<Post> getAllPosts() {
         return PostManager.getInstance().getAllPosts();
     }
 
+    /**
+     * Method to get the user
+     * @param username the username of the user to be retrieved
+     * @param password the password of the user to be retrieved
+     * @return the user if found
+     */
     public User getUser(String username, String password) {
         User user = userManager.getUser(username);
         if (user != null && user.getPassword().equals(password)) {
@@ -42,15 +68,38 @@ public class InterviewApp {
         return null;
     }
 
+    /**
+     * Method to create a user with the given attributes
+     * @param username the username of the user to be created
+     * @param email the email of the usr to be created
+     * @param password the password of the user to be created
+     * @param firstName the first name of the user to be created
+     * @param lastName the last name of the user to be created
+     * @param role the role of the user to be created
+     * @param major the major of the user to be created
+     * @param year the year of the user to be created 
+     * @return true if the user was successfuly created, false otherwise
+     */
     public boolean createUser(String username, String email, String password, String firstName, String lastName, Role role, Major major, Year year)
     {
        return userManager.addUser(username, email, password, firstName, lastName, role, major, year);
     }
 
+    /**
+     * Method to check if a user was logged in
+     * @param username the username of the user to login
+     * @param email the email of the user to login
+     * @param password the password of the user to loin
+     * @return true if the user was successfuly logged in
+     */
     public boolean login(String username, String email, String password) {
         return userManager.login(username, email, password);
     }
 
+    /**
+     * Method to check if a user successfuly logged out
+     * @return true if a user was seuccessfuly logged out, false otherwise
+     */
     public boolean logout()
     {
         return userManager.logout();
@@ -73,46 +122,87 @@ public class InterviewApp {
         return false;
     }
 
+    /**
+     * Method to check of a question was add
+     * @param question the question to be added
+     * @return true if the question was successful added
+     */
     public boolean addQuestion (QuestionPost question)
     {
         return postManager.addQuestion(question);
 
     }
 
+    /**
+     * Method to check if a solution was successfuly added
+     * @param question the question to be added with the solution
+     * @param user the user that wants to add a soltuion
+     * @param solution the solution to be added
+     * @return true if the solution was added
+     */
     public boolean addSolution(QuestionPost question, User user, SolutionPost solution)
     {
         return postManager.addSolution(question, user, solution);
 
     }
 
+    /**
+     * Method to get the question
+     * @param title the title of the question
+     * @return the question
+     */
     public ArrayList<QuestionPost> getQuestion(String title)
     {
         return postManager.getQuestion(title);
 
     }
 
+    /**
+     * Method to add a saved question
+     * @return true if the question was saved
+     */
     public boolean addSavedQuestion()
     {
         return postManager.save();
 
     }
 
+    /**
+     * Method to add a comment to a post
+     * @param post the post the comment is to be added to 
+     * @param comment the comment to be added
+     * @return the comment
+     */
     public boolean addComment(Post post,Comment comment)
     {
         return postManager.addComment(post, comment);
     }
 
+    /**
+     * Method to get the comment
+     * @param post the post the comment is on
+     * @return the comment
+     */
     public ArrayList<Comment> getComments(Post post)
     {
         return postManager.getComments(post);
     }
 
+    /**
+     * Method to remove a saved question
+     * @return true if the question was successfuly removed, false otherwise
+     */
     public boolean removeSavedQuestion()
     {
         return postManager.save();
 
     } 
 
+    /**
+     * Method to check if the system has a user 
+     * @param username the username of the user to check for
+     * @return the user if the user is in the system
+     */
     public boolean haveUser(String username) 
     {
     return userManager.userExist(username);
