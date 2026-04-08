0875b4637445affd3190f186ff2e030e1e342ca4
GitGud9275 <tjhamilton738@gmail.com>
2026-04-08 13:13:17 -0400

    Updated styles.css

--- src/main/java/com/interviewapp/App.java
+++ src/main/java/com/interviewapp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("intro"), 1440, 900);
+        scene = new Scene(loadFXML("intro"), 1440, 800);
         stage.setScene(scene);
         stage.show();
     }
