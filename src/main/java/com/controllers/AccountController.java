a296c1bfefe6b6d51a3319234b7d416630de797a
MyilaHoward <myila@email.sc.edu>
2026-04-12 18:14:07 -0400

    added AccountController class

---
+++ src/main/java/com/controllers/AccountController.java
@@ -0,0 +1,27 @@
+package com.controllers;
+
+import com.model.UserManager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+
+public class AccountController {
+    @FXML private TextField usernameField;
+    @FXML private TextField emailField;
+    @FXML private PasswordField passwordField;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField majorField;
+    @FXML private TextField roleField;
+    @FXML private Label errorLabel;
+
+
+    private UserManager userManager = UserManager.getInstance();
+
+    
+
+}
