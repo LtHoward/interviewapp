4ec479c236c4c32299f1aac87e281d46ac1226d4
MyilaHoward <myila@email.sc.edu>
2026-04-21 14:24:01 -0400

    add any changes

--- src/main/java/com/controllers/SignupController.java
+++ src/main/java/com/controllers/SignupController.java
@@ -1,11 +1,14 @@
 package com.controllers;
 
+import java.io.IOException;
+
 import com.model.UserManager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 
 
@@ -18,10 +21,21 @@ public class SignupController {
     @FXML private TextField majorField;
     @FXML private TextField roleField;
     @FXML private Label errorLabel;
+    @FXML private Button signinbutton;
+    @FXML private Button loginbutton;
 
 
     private UserManager userManager = UserManager.getInstance();
 
-    
+    @FXML 
+    private void handlesignup() throws IOException {
+        String username = usernameField.getText();
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        String firstname = firstNameField.getText();
+        String lastname = lastNameField.getText();
+        String 
+        
+    }
 
 }
