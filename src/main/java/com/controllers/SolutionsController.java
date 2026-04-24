932d2d14d8fb943dcfbaee6de8a014b74d5d352b
Dorian Rhone <drhone@email.sc.edu>
2026-04-24 17:03:37 -0400

    adding back method for solutions page #166

--- src/main/java/com/controllers/SolutionsController.java
+++ src/main/java/com/controllers/SolutionsController.java
@@ -1,5 +1,28 @@
 package com.controllers;
 
+import com.model.User;
+import com.interviewapp.App;
+import com.model.QuestionPost;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 public class SolutionsController {
     
+    private User currentUser;
+
+    private QuestionPost selectedQuestion;
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = App.setRootWithLoader("questionPost");
+            QuestionsController controller = loader.getController();
+            controller.setData(currentUser, selectedQuestion);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }   
+
 }
